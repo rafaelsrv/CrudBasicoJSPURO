c07f323833e0b44ea2fc5cfbd05258036f3508ea
RafaelSRV <rafael-srv@hotmail.com>
2023-01-24 23:33:58 -0300

    CrudBásico

---
+++ javascript/js/script1.js
@@ -0,0 +1,20 @@
+class Produto{
+
+    constructor(){
+        this.id = 0;
+        this.nomeProduto = "";
+        this.valor = 0;
+
+
+    }
+    adicionar(){
+        alert('Vamos adicionar um produto')
+        
+        
+    }
+    excluir(){
+        alert('Vamos deletar um produto')
+    }
+}
+
+var produto = new Produto()
