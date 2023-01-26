4af7204472f03c728a25025398a5a784af900e28
RafaelSRV <rafael-srv@hotmail.com>
2023-01-26 18:15:30 -0300

    Pasta Principal

--- js/script1.js
+++ js/script1.js
@@ -1,19 +1,54 @@
 class Produto{
 
     constructor(){
-        this.id = 0;
-        this.nomeProduto = "";
-        this.valor = 0;
-        this.status = true;
+        this.id = 1;
+        this.arrayProdutos = [];
 
 
     }
-    adicionar(){
-        alert('Vamos adicionar um produto')
+    salvar(){
         
+        let produto = this.lerDados()
+
+        if(this.validaCampos(produto)){
+            this.adicionar(produto);
+        }
+    }   
+    adicionar(produto){
+        this.arrayProdutos.push(produto);
+        this.id++
+        console.log(produto)
+
+    }    
+    
+    lerDados(){
+        let produto = {}
         
+        produto.id = this.id
+        produto.nomeProduto = document.getElementById('produto').value
+        produto.preco = document.getElementById('preco').value
+        
+        return produto;
     }
-    excluir(){
+    validaCampos(produto){
+        let msg = ''
+        if(produto.nomeProduto == ''){
+            msg += '-Informe o Nome do Produto \n'
+        }
+        
+        if(produto.preco == ''){
+            msg += '-Informe o Preço do Produto \n'
+        }
+        if(msg !=''){
+            alert(msg)
+            return false
+        }
+        return true
+
+    }
+
+    
+    cancelar(){
         alert('Vamos deletar um produto')
     }
 }
